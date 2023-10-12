c9d1b1ed8b8555fa3ebdfc7049a7d5f2e8c9acbb
Boccio04 <alessandrobocedi2004@gmail.com>
2023-10-12 17:25:51 +0200

    App

---
+++ Class1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prima_app
+{
+    class Auto
+    {
+        // Tipi personalizzati
+
+        public enum motore
+        {
+            benzina,
+            diesel,
+            elettrico
+        }
+
+        // Proprietà
+        /// <summary>
+        /// Marca del veicolo
+        /// </summary>
+        public string? marca;
+        public string? modello;
+        public string? colore;
+        public motore tipoMotore;
+
+        public int livelloCarburante;
+        private bool accesa;
+
+        //costruttore
+        public Auto()
+        {
+        }
+
+        public Auto(string? marca, string? modello, string? colore, motore tipoMotore, int livelloCarburante)
+        {
+            this.marca = marca;
+            this.modello = modello;
+            this.colore = colore;
+            this.tipoMotore = tipoMotore;
+            this.livelloCarburante = livelloCarburante;
+        }
+
+        /// <summary>
+        /// Stampa la descrizione del veicolo
+        /// </summary>
+        public void StampaDescrizione()
+        {
+            //stampo le caratteristiche dell'auto
+            Console.WriteLine(generaDescrizione());
+        }
+
+
+
+        private string generaDescrizione()
+        {
+
+            string result;
+
+            result = "marca:" + this.marca + "\n";
+            result += $"modello: {this.modello} \n";
+            result += $"motore:  {this.tipoMotore}\n";
+            result += $"colore:  {this.colore}\n";
+            result += "livello Carburante:" + this.livelloCarburante;
+
+            return result;
+
+        }
+
+        /// <summary>
+        /// Genera lo stato (carburante, accensione,) del veicolo
+        /// </summary>
+        /// <returns>Lo stato del veicolo</returns>
+        /// 
+
+        public void stampaStato()
+        {
+            Console.WriteLine(GeneraStato());
+        }
+
+        public string GeneraStato()
+        {
+            string result;
+
+            result = $"carburante: {this.livelloCarburante}";
+            if (this.accesa == true)
+            {
+                // se il veicolo è acceso scrivo accesa
+                result += "accesa";
+
+            }
+            else
+            {
+                // alrtrimenti scrivo spenta
+                result += "spenta";
+
+
+            }
+
+
+
+
+            return result;
+
+        }
+
+        /// <summary>
+        /// Tenta di avviare il veicolo <para>
+        /// Se non c'è carburante l'accensione fallisce</para>
+        /// </summary>
+        public void accendi()
+        {
+
+            //verifio il livello del carburante
+            if (this.livelloCarburante > 0)
+            {
+                //se è maggiore di zero accendo il veicolo
+                this.accesa = true;
+            }
+            else
+            {
+                //se è minore o uguale a zero spengo il veicolo
+                this.accesa = false;
+            }
+
+
+        }
+
+        /// <summary>
+        /// Spegne il veicolo
+        /// </summary>
+
+        public void spegni()
+        {
+            this.accesa = false;
+            // spegne il veicolo
+
+        }
+
+
+    }
+
+    // fine
+}
--- Program.cs
+++ Program.cs
@@ -6,78 +6,36 @@
 // All'avvio dell'app deve mostrare lo stato dell'auto, stampando marca, modello, colore e tipo di motore
 // Per modellare l'automobile implementiamo una classe chiamata auto
 
+using System.Runtime.CompilerServices;
+using prima_app;
+
 Auto miaAuto;
 
-miaAuto = new Auto("Ferrari", "Purosangue", "rosso", Auto.motore.elettrico);
+miaAuto = new Auto("Ferrari", "Purosangue", "rosso", Auto.motore.elettrico, 0);
 
 
+
+miaAuto.StampaDescrizione();
+miaAuto.stampaStato();
+
+
+miaAuto.accendi();
+miaAuto.stampaStato();
+
+miaAuto.spegni();
+miaAuto.stampaStato();
+
+Console.ReadLine();
 
 //miaAuto.marca = "ferrari";
 //miaAuto.modello = "purosangue";
 //miaAuto.colore = "rosso";
 //miaAuto.tipoMotore = Auto.motore.benzina;
 
-miaAuto.StampaDescrizione();
-
-Console.ReadLine();
 
 
-class Auto{
-    // Tipi personalizzati
-
-    public enum motore {
-        benzina,
-        diesel,
-        elettrico
-    }
-
-    // Proprietà
-    /// <summary>
-    /// Marca del veicolo
-    /// </summary>
-    public string? marca;
-    public string? modello;
-    public string? colore;
-    public motore tipoMotore;
-
-    //costruttore
-    public Auto()
-    {
-    }
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="marca">Marca del veicolo (es;fiat)</param>
-    /// <param name="modello">Modello del veicolo (es:panda)</param>
-    /// <param name="colore">Colore del veicolo</param>
-    /// <param name="tipoMotore">Motore del veicolo(es:benzina,diesel,elettrica)</param>
-
-    public Auto(string? marca, string? modello, string? colore, motore tipoMotore)
-    {
-        this.marca = marca;
-        this.modello = modello;
-        this.colore = colore;
-        this.tipoMotore = tipoMotore;
-    }
-
-    public void StampaDescrizione()
-    {
-        //stampo le caratteristiche dell'auto
-        Console.WriteLine(generaDescrizione());
-    }
-
-    private string generaDescrizione(){
-
-        string result;
-
-        result = "marca:" + this.marca + "\n";
-        result += $"modello: {this.modello} \n";
-        result += $"motore:  {this.tipoMotore}\n";
-        result += "colore:" + this.colore;
-
-        return result;
-
-    }
 
 
-}
+
+
+
