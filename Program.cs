b2d37949f0126150c82ad7b157494aa03fc7def4
Boccio04 <alessandrobocedi2004@gmail.com>
2023-10-12 13:01:47 +0200

    Ferrari

--- Program.cs
+++ Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// L'app deve modellare un'automobile, per farlo deve registrare le informazioni relative a:
+// - marca
+// - modello
+// - tipo di motore
+// - colore 
+// All'avvio dell'app deve mostrare lo stato dell'auto, stampando marca, modello, colore e tipo di motore
+// Per modellare l'automobile implementiamo una classe chiamata auto
+
+Auto miaAuto;
+
+miaAuto = new Auto();
+
+miaAuto.marca = "ferrari";
+miaAuto.modello = "purosangue";
+miaAuto.colore = "rosso";
+miaAuto.tipoMotore = Auto.motore.benzina;
+
+miaAuto.stampa();
+
+Console.ReadLine();
+
+
+class Auto{
+    // Tipi personalizzati
+
+    public enum motore {
+        benzina,
+        diesel,
+        elettrico
+    }
+
+    // Proprietà
+    public string? marca;
+    public string? modello;
+    public string? colore;
+    public motore tipoMotore;
+
+    public Auto()
+    {
+    }
+
+    public Auto(string? marca, string? modello, string? colore, motore tipoMotore)
+    {
+        this.marca = marca;
+        this.modello = modello;
+        this.colore = colore;
+        this.tipoMotore = tipoMotore;
+    }
+
+    public void stampa(){
+        // stampo le caratteristiche dell'auto
+        Console.WriteLine("marca: "+ this.marca);
+        Console.WriteLine("modello: "+ this.modello);
+        Console.WriteLine("motore: "+ this.tipoMotore);
+        Console.WriteLine("colore: "+ this.colore);
+
+    }
+
+
+}
